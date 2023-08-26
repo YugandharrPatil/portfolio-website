9af3af256bee22d6ed2672ec551715c92dc30938
Yugandhar Patil <yugandhar.patil@learner.manipal.edu>
2023-08-26 19:36:11 +0530

    fixed database not working(hopefully)

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,13 @@
 import { connect } from "@planetscale/database";
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
 // create database connection
 const connection = connect({
 	url: process.env.DATABASE_URL,
+	host: process.env.DATABASE_HOST,
+	username: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
 });
 
 export const db = drizzle(connection);
