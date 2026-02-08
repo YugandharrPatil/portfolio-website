e71c6dad988d03dd28767b3f2f46be891f1d88df
YugandharrPatil <yugandhar.patil@learner.manipal.edu>
2026-02-08 22:21:07 +0530

    github auth token updated

--- src/lib/utils/calls.ts
+++ src/lib/utils/calls.ts
@@ -6,7 +6,7 @@ export default async function fetchRepos() {
       "https://api.github.com/users/YugandharrPatil/repos?per_page=1000",
       {
         headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_AUTH_TOKEN}`,
+          Authorization: `Bearer ${process.env.GITHUB_AUTH_TOKEN}`,
           "X-GitHub-Api-Version": "2022-11-28",
           Accept: "application/vnd.github+json",
         },
